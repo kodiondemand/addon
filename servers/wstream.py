0ee1fc47a29d83897636f9fabc24c31df32068e0
Dexter Morgan <9434916+32Dexter@users.noreply.github.com>
2019-06-09 19:01:40 +0200

    Update Wstream Server
    Delete voice "vid"

--- servers/wstream.py
+++ servers/wstream.py
@@ -25,7 +25,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
 
     data = httptools.downloadpage(page_url, headers=headers).data.replace('https', 'http')
     # logger.info("[wstream.py] data=" + data)
-    vid = scrapertools.find_multiple_matches(data, 'download_video.*?>.*?<.*?<td>([^\,,\s]+)')
+    #vid = scrapertools.find_multiple_matches(data, 'download_video.*?>.*?<.*?<td>([^\,,\s]+)')
 
     headers.append(['Referer', page_url])
     post_data = scrapertools.find_single_match(data,"</div>\s*<script type='text/javascript'>(eval.function.p,a,c,k,e,.*?)\s*</script>")
@@ -34,14 +34,14 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
         data = jsunpack.unpack(post_data)
 
     block = scrapertools.find_single_match(data, 'sources:\s*\[[^\]]+\]')
-    if block: data = block
 
+    if block: data = block
     media_urls = scrapertools.find_multiple_matches(data, '(http.*?\.mp4)')
     _headers = urllib.urlencode(dict(headers))
     i = 0
 
     for media_url in media_urls:
-        video_urls.append([vid[i] + " mp4 [wstream] ", media_url + '|' + _headers])
+        video_urls.append(["mp4 [wstream] ", media_url + '|' + _headers])
         i = i + 1
 
     for video_url in video_urls:
