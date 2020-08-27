176b04c64b6807cc04f58375a18471e582b08df8
marco <m.toma99@gmail.com>
2020-08-27 11:39:51 +0200

    potenziato match tmdb e alcuni fix per la ricerca globale

--- specials/search.py
+++ specials/search.py
@@ -172,7 +172,6 @@ def channel_search(item):
     valid = list()
     ch_list = dict()
     mode = item.mode
-    max_results = 10
 
     if item.infoLabels['tvshowtitle']:
         item.text = item.infoLabels['tvshowtitle'].split('-')[0]
@@ -262,8 +261,8 @@ def channel_search(item):
         ch_name = channel_titles[channel_list.index(key)]
         grouped = list()
         cnt += 1
-        progress.update(old_div((cnt * 100), len(ch_list)), config.get_localized_string(60295), config.get_localized_string(60293))
-        if len(value) <= max_results and item.mode != 'all':
+        progress.update(old_div((cnt * 100), len(ch_list)), config.get_localized_string(60295) + '\n' + config.get_localized_string(60293))
+        if item.mode != 'all':
             if len(value) == 1:
                 if not value[0].action or config.get_localized_string(70006).lower() in value[0].title.lower():
                     continue
