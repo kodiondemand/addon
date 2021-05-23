991849b416c083c2a3270d00b84b10ecd0792b0e
fatshotty <fat@fatshotty.net>
2021-05-23 21:18:16 +0200

    contextmenu in Kod Options

---
+++ contextmenu.py
@@ -0,0 +1,78 @@
+import os
+from platformcode import config, logger
+import xbmc, sys, xbmcgui, os
+
+
+librerias = xbmc.translatePath(os.path.join(config.get_runtime_path(), 'lib'))
+sys.path.insert(0, librerias)
+
+from core import jsontools
+
+addon_id = config.get_addon_core().getAddonInfo('id')
+
+LOCAL_FILE = os.path.join(config.get_runtime_path(), "contextmenu.json")
+f = open(LOCAL_FILE)
+try:
+    contextmenu_settings = jsontools.load( f.read() )
+except:
+    contextmenu_settings = []
+f.close()
+
+
+
+
+def build_menu():
+
+    tmdbid = xbmc.getInfoLabel('ListItem.Property(tmdb_id)')
+    mediatype = xbmc.getInfoLabel('ListItem.DBTYPE')
+    title = xbmc.getInfoLabel('ListItem.Title')
+    year = xbmc.getInfoLabel('ListItem.Year')
+    imdb = xbmc.getInfoLabel('ListItem.IMDBNumber')
+    filePath = xbmc.getInfoLabel('ListItem.FileNameAndPath')
+    containerPath = xbmc.getInfoLabel('Container.FolderPath')
+
+    logstr = "Selected ListItem is: 'IMDB: {}' - TMDB: {}' - 'Title: {}' - 'Year: {}'' - 'Type: {}'".format(imdb, tmdbid, title, year, mediatype)
+    logger.info(logstr)
+    logger.info(filePath)
+    logger.info(containerPath)
+
+    contextmenumodules = []
+    contextmenu = []
+    
+    for itemmodule in contextmenu_settings:
+        logger.debug('check contextmenu {}'.format(itemmodule) )
+        module = __import__(itemmodule, None, None, [ itemmodule] )
+
+        if module.check_condition():
+            logger.info('Add contextmenu item -> {}'.format(itemmodule) )
+            contextmenumodules.append( module )
+
+
+    contextmenu = []
+    empty = False
+    if len(contextmenumodules) == 0:
+        logger.info('No contextmodule found, build an empty one')
+        contextmenu.append( empty_item() )
+        empty = True
+    else:
+        for itemmodule in contextmenumodules:
+            contextmenu.append( itemmodule.get_menu_item() )
+    
+    ret = xbmcgui.Dialog().contextmenu( contextmenu )
+    
+    if not empty and ret > -1:
+        itemmodule = contextmenumodules[ ret ]
+        logger.info( 'Contextmenu module index {} for -> {}'.format(ret, itemmodule) )
+        itemmodule.execute()
+
+
+def empty_item():
+    return config.get_localized_string(90004)
+
+
+if __name__ == '__main__':
+    build_menu()
+
+
+
+    
---
+++ core/contextmenu/search_on_kod.py
@@ -0,0 +1,109 @@
+import xbmc, sys, xbmcgui, os
+from platformcode import config, logger
+
+# incliuding folder libraries
+librerias = xbmc.translatePath(os.path.join(config.get_runtime_path(), 'lib'))
+sys.path.insert(0, librerias)
+
+
+from core import tmdb
+from core.item import Item
+
+addon_id = config.get_addon_core().getAddonInfo('id')
+
+
+def check_condition():
+    logger.debug('[SOK] check item condition')
+    mediatype = xbmc.getInfoLabel('ListItem.DBTYPE')
+
+    folderPath = xbmc.getInfoLabel('Container.FolderPath')
+    filePath = xbmc.getInfoLabel('ListItem.FileNameAndPath')
+
+    logger.debug('Container: {}'.format(folderPath) )
+    logger.debug('listitem mediatype: {}'.format(mediatype) )
+    logger.debug('filenamepath: {}'.format(filePath) )
+
+    # we_are_in_kod = folderPath.find( addon_id ) > -1
+    item_is_coming_from_kod = filePath.find( addon_id ) > -1
+
+    # logger.info('[SOK] container is KOD? {}'.format(we_are_in_kod) )
+
+    return mediatype and not item_is_coming_from_kod # and not we_are_in_kod
+
+def get_menu_item():
+    logger.debug('get menu item')
+    return config.get_localized_string(90005)
+
+
+def execute():
+    """
+    Gather the selected ListItem's attributes in order to compute the `Item` parameters
+    and perform the KOD's globalsearch.
+    Globalsearch will be executed specifing the content-type of the selected ListItem
+
+    NOTE: this method needs the DBTYPE and TMDB_ID specified as ListItem's properties
+    """
+
+    # These following lines are commented and keep in the code just as reminder.
+    # In future, they could be used to filter the search outcome
+
+    # ADDON: maybe can we know if the current windows is related to a specific addon?
+    # we could skip the ContextMenu if we already are in KOD's window
+
+    tmdbid = xbmc.getInfoLabel('ListItem.Property(tmdb_id)')
+    mediatype = xbmc.getInfoLabel('ListItem.DBTYPE')
+    title = xbmc.getInfoLabel('ListItem.Title')
+    year = xbmc.getInfoLabel('ListItem.Year')
+    imdb = xbmc.getInfoLabel('ListItem.IMDBNumber')
+
+    logstr = "Selected ListItem is: 'IMDB: {}' - TMDB: {}' - 'Title: {}' - 'Year: {}'' - 'Type: {}'".format(imdb, tmdbid, title, year, mediatype)
+    logger.info(logstr)
+
+    if not tmdbid and imdb:
+        logger.info('No TMDBid found. Try to get by IMDB')
+        it = Item(contentType= mediatype, infoLabels={'imdb_id' : imdb})
+        try:
+            tmdb.set_infoLabels(it)
+            tmdbid = it.infoLabels.get('tmdb_id', '')
+        except:
+            logger.info("Cannot find TMDB via imdb")
+
+    if not tmdbid:
+        logger.info('No TMDBid found. Try to get by Title/Year')
+        it = Item(contentTitle= title, contentType= mediatype, infoLabels={'year' : year})
+        try:
+            tmdb.set_infoLabels(it)
+            tmdbid = it.infoLabels.get('tmdb_id', '')
+        except:
+            logger.info("Cannot find TMDB via title/year")
+
+
+    if not tmdbid:
+        # We can continue searching by 'title (year)'
+        logger.info( "No TMDB found, proceed with title/year: {} ({})".format(title,  year) )
+
+
+    logger.info("Search on KOD (gobalsearch)")
+
+    item = Item(
+    action="Search",
+    channel="globalsearch",
+    contentType= mediatype,
+    mode="search",
+    text= title,
+    type= mediatype,
+    infoLabels= {
+        'tmdb_id': tmdbid,
+        'year': year
+    },
+    folder= False
+    )
+
+    logger.info("Invoking Item: {}".format(item.tostring()))
+
+    itemurl = item.tourl()
+    xbmc.executebuiltin("RunPlugin(plugin://plugin.video.kod/?" + itemurl + ")")
+
+
+
+
