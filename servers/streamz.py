66def5c3a16927eea1df0db4cc27e722a6418360
Alhaziel01 <alhaziel01@gmail.com>
2021-07-06 17:26:09 +0200

    Fix streamZ

--- servers/streamz.py
+++ servers/streamz.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # --------------------------------------------------------
-# Conector Streamz By Alfa development Group
+# Conector Streamz
 # --------------------------------------------------------
 
 import re
-from core import httptools
+from core import httptools, scrapertools, support
 from platformcode import logger, config
+from lib import jsunpack
 
 
 def test_video_exists(page_url):
@@ -14,16 +15,26 @@ def test_video_exists(page_url):
     data = httptools.downloadpage(page_url)
 
     if "<b>File not found, sorry!</b" in data.data:
-        return False, config.get_localized_string(70449) % "streamz"
+        return False, config.get_localized_string(70449) % "streamZ"
     return True, ""
 
 
 def get_video_url(page_url, video_password=""):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
+    packed_data = scrapertools.find_multiple_matches(data.data, r'(eval.*?video(\d).*?video_(\d)[^<]+)')
+    if packed_data:
+        for p, index, control in packed_data:
+            if index == control:
+                try:
+                    url = scrapertools.find_single_match(jsunpack.unpack(p), r"src:\\'([^'\\]+)")
+                    break
+                except:
+                    pass
 
-    url = re.sub(r'(\.\w{2,3})/\w', '\\1/getlink-', data.url) + '.dll'
-    url += "|User-Agent=%s" % httptools.get_user_agent()
-    video_urls.append([".mp4 [Streamz]", url])
+    else:
+        url = re.sub(r'(\.\w{2,3})/\w', '\\1/getl1nk-', data.url) + '.dll'
+    url += "|Referer=https://streamz.ws/&User-Agent=Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14'"
+    video_urls.append(["mp4 [streamZ]", url])
 
     return video_urls
