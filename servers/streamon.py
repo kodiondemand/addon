12c40d3686691b72f27117c78525e7e7f5c4d7f1
fatshotty <fat@fatshotty.net>
2021-07-06 18:21:07 +0200

    fix: use tabber.js in order to retrieve variables

--- servers/streamon.py
+++ servers/streamon.py
@@ -27,16 +27,34 @@ def exec_Js(str):
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
 
+  support.dbg()
+
   first_decoder_js = scrapertools.find_single_match(htmldata, '<script\s+?type=[\'|"].*?[\'|"]>\s?(var.*?)<\/script>')
 
   first_decoder_js = first_decoder_js.replace('eval', '')
 
   first_decoder_fn = exec_Js(first_decoder_js)
 
-  variable_value = scrapertools.find_single_match(first_decoder_fn, 'var eeefdbadffde="(.*?)"')
 
-  res = variable_value.replace("YjZkZTU4OGFmODJhOTE4Y2FjMWUzZmYyNmQyOWRkMGY", "")
-  res2 = res.replace("ZWY4OGY0MjRlNDY3NGU4MzgzZDQ1YjMxNTdkYzc2MmY=", "")
+  tabber_js = httptools.downloadpage('https://streamon.to/assets/js/tabber.js').data
+  tabber_js = tabber_js.replace('eval', '')
+  tabber_js_fn = exec_Js(tabber_js)
+
+  groups = scrapertools.find_multiple_matches( tabber_js_fn, 'var res\s+?=\s+?(.*?)\.replace\("(.*?)"\).*\.replace\("(.*?)"\).*atob\(res2\)' )
+  
+  variable = None
+  key1 = None
+  key2 = None
+  if len(groups) > 0:
+    groups = groups[0]
+    variable = groups[0]
+    key1 = groups[1][ :-4]
+    key2 = groups[2][ :-4]
+
+  variable_value = scrapertools.find_single_match(first_decoder_fn, 'var {}="(.*?)"'.format(variable) )
+
+  res = variable_value.replace(key1, "")
+  res2 = res.replace(key2, "")
   media_url = base64.b64decode( res2 ).decode('ascii')
   
   video_urls = []
