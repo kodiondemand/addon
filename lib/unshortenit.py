e21f02a99869dc73f7f08f476444fa574f35c442
TheHacker66 <thehacker66@gmail.com>
2024-03-21 21:25:40 +0100

    Fix

--- lib/unshortenit.py
+++ lib/unshortenit.py
@@ -710,7 +710,8 @@ class UnshortenIt(object):
 
     def _unshorten_uprot(self, uri):
         html = httptools.downloadpage(uri, cloudscraper=False).data
-        link = scrapertools.find_single_match(html, r'--></button></[a|div]?>.+?<a[^>]+href="([^"]+)">')
+        link = scrapertools.find_single_match(html, r'</[button|a]>.+<a[^>]+href="([^"]+)">.+C o')
+        #import web_pdb; web_pdb.set_trace()
         if link != uri:
             return link, 200
         return uri, 200
