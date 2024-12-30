d9f04d330129ce64db50d747d6bdc547dc5655ed
Luca <luca.puglia89@gmail.com>
2024-12-30 20:18:24 +0100

    fixed tg logo

--- channels/la7.py
+++ channels/la7.py
@@ -81,9 +81,10 @@ def live(item):
 
 def replay_channels(item):
     itemlist = [item.clone(title=support.typo('La7', 'bold'), fulltitle='La7', url= host + '/rivedila7/0/la7', action='replay_menu', forcethumb = True),
-                item.clone(title=support.typo('La7d', 'bold'), fulltitle='La7d', url= host + '/rivedila7/0/la7d', action='replay_menu', forcethumb = True),
-                item.clone(title=support.typo('TG La7', 'bold'), fulltitle='TG La7', url= host + '/tgla7', action='episodios', forcethumb = True)]
-    return support.thumb(itemlist, live=True)
+                item.clone(title=support.typo('La7d', 'bold'), fulltitle='La7d', url= host + '/rivedila7/0/la7d', action='replay_menu', forcethumb = True)]
+    itemlist = support.thumb(itemlist, live=True)
+    itemlist.append(item.clone(title=support.typo('TG La7', 'bold'), fulltitle='TG La7', url= host + '/tgla7', action='episodios', thumbnail='https://tg.la7.it/sites/all/themes/bootstrap/img/logo_mod.svg'))
+    return itemlist
 
 
 @support.scrape
